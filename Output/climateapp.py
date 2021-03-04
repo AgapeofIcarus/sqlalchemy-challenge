f991c9c3939438efc97886c250f482d7a7ace166
Chase Webb <webb.chase.a@gmail.com>
2021-03-04 16:50:07 -0500

    created app for analysis

---
+++ Output/climateapp.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return(
+     f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
