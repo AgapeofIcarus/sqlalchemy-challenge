245aad9efda3eb418e563fc4113b4efa2d8e6a77
Chase Webb <webb.chase.a@gmail.com>
2021-03-04 17:33:47 -0500

    connected database to app

--- Output/climateapp.py
+++ Output/climateapp.py
@@ -1,8 +1,24 @@
-### Import Flash and jsonify ###
+### Do imports ###
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
 
-### Create dictionaries ###
-precipitation = []
+### Set up Database connection ###
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the tables
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 ### Set up Flask ###
 app = Flask(__name__)
