d9076cbc74004f5231d60bbd2eef6fa14150523f
Chase Webb <webb.chase.a@gmail.com>
2021-03-04 19:42:59 -0500

    Update climateapp.py
    Added routes to app code.

--- Output/climateapp.py
+++ Output/climateapp.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
@@ -37,6 +37,62 @@ def home():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    """Return precipitation data as json"""
+     # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #Query prcp data.
+    results = session.query(measurement.date, measurement.prcp).\
+    filter(measurement.date <= '2017-08-23').\
+    filter(measurement.date >= '2016-08-23').\
+    order_by(measurement.date.desc()
+
+    session.close()
+    
+    #Create dictionary from the query
+    precipitation = []
+
+    for date, prcp in results:
+        precipt= {}
+        precipt["date"] = date
+        precipt["prcp"] = prcp
+        precipitation.append(precipt)
 
     return jsonify(precipitation)
+
+@app.route("/api/v1.0/stations")
+    station_results = session.query(station.station, station.name).all()
+
+    session.close()
+
+    #create dictionary from the query
+    stations = []
+
+    for station, name in station_results:
+        stns= {}
+        stns["station"] = station
+        stns["name"] = name
+        stations.append(stns)
+
+@app.route("/api/v1.0/tobs")
+    temps = session.query(measurement.tobs).\
+    filter(measurement.date <= '2017-08-23').\
+    filter(measurement.date >= '2016-08-23').\
+    filter(measurement.station == 'USC00519281').all()
+
+    session.close()
+
+    #create dictionary from the query
+    temperatures = []
+
+    for tobs in temps:
+        temp= {}
+        temp["date"] = date
+        temp["tobs"] = tobs
+        temperatures.append(temp)
+
+@app.route("/api/v1.0/<start>")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
